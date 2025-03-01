3b6a24ca814c49bb2602b54bf919c08c582730b7
tbw <2725989503@qq.com>
2025-03-01 09:59:43 +0800

    Second Practice,konw about basic process

--- SimpleNet.py
+++ SimpleNet.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
@@ -13,14 +15,29 @@ class SimpleNet(nn.Module):
         x = self.fc2(x)
         return x
 
-
-# 初始化网络
 model = SimpleNet()
+x = torch.randn(1000,4)
+y= torch.randint(0,2,(1000,))
+dataset=TensorDataset(x,y)
+train_loader = DataLoader(dataset,batch_size=32,shuffle=True)
+criterion = nn.CrossEntropyLoss()#损失函数
+optimizer = optim.Adam(model.parameters(),lr = 0.01)
+simgle_sample = x[0:1]
+simgle_label = y[0:1]
+for epoch in range(100):
+    for i, (inputs, labels) in enumerate(train_loader):
+        # 前向传播
+        outputs = model(simgle_sample)
+        loss = criterion(outputs, simgle_label)
+        
+        # 反向传播与优化
+        optimizer.zero_grad()  # 清空梯度（必须！否则梯度会累积）
+        loss.backward()        # 计算梯度（链式法则）
+        optimizer.step()       # 更新参数（根据梯度下降）
+        
+        #打印训练信息
+        if (i+1) % 10 == 0:
+           print(f'Epoch [{epoch+1}/{10}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+        #print(f"Loss:{loss.item()} time:{epoch} ")
 
-# 随机生成输入数据（2个样本，每个样本4个特征）
-inputs = torch.randn(2, 4)  # 形状：[batch_size, input_size]
 
-# 前向传播
-outputs = model(inputs)
-print(outputs)
-# 输出形状：[2, 2]，表示2个样本，每个样本对应2个类别的得分
